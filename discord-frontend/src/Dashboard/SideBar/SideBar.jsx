437317e7a11817a86ee495f6c719e82c9a26daa4
assel1021 <111820267+assel1021@users.noreply.github.com>
2023-11-07 20:33:06 -0600

    Corrections Video Call

--- discord-frontend/src/Dashboard/SideBar/SideBar.jsx
+++ discord-frontend/src/Dashboard/SideBar/SideBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {styled} from '@mui/system';
-import MainPageButton from './MainPageButton';
 import Button from '@mui/material/Button';
 import GroupsIcon from '@mui/icons-material/Groups';
 import PersonIcon from '@mui/icons-material/Person';
 import CreateRoomButton from './CreateRoomButton';
+import { connect } from 'react-redux';
+import Avatar from '../../shared/components/Avatar';
+import { Tooltip } from '@mui/material';
+
+//import MainPageButton from './MainPageButton';
 
 const MainContainer=styled('div')({
     width:'72px',
@@ -15,9 +19,47 @@ const MainContainer=styled('div')({
     //backgroundColor:'#202225'
     backgroundColor:"rgba(255, 255, 255,0.1)",
     borderRadius:"20px",
-})
+});
 
-const SideBar = ({ onPrivateChatsClick, onGroupChatsClick }) => {
+const ActiveRoomButton = ({
+    creatorUsername,
+    roomId,
+    amountOfParticipants,
+    isUserInRoom,
+}) => {
+    const handleJoinActiveRoom = () => {
+        if(amountOfParticipants < 4){
+            //Join room
+        }
+    }
+
+    const activeRoomButtonDisabled = amountOfParticipants > 3;
+    const roomTitle = `Cretor: ${creatorUsername}. Connected: ${amountOfParticipants}`;
+
+    return <Tooltip title={roomTitle}>
+        <div>
+            <Button
+                style={{
+                    width:'48px',
+                    height:'48px',
+                    borderRadius:'16px',
+                    margin:0,
+                    padding:0,
+                    minWidth:0,
+                    marginTop:'10px',
+                    color:'white',
+                    backgroundColor:'#5865F2'
+                }}
+                disabled={activeRoomButtonDisabled || isUserInRoom}
+                onClick={handleJoinActiveRoom}
+            >
+                <Avatar username={creatorUsername} />
+            </Button>
+        </div>
+    </Tooltip>
+};
+
+const SideBar = ({ activeRooms, isUserInRoom, onPrivateChatsClick, onGroupChatsClick }) => {
     return (
         <MainContainer>
             <Button
@@ -53,9 +95,24 @@ const SideBar = ({ onPrivateChatsClick, onGroupChatsClick }) => {
             <GroupsIcon/>  
             </Button>
             <CreateRoomButton/>
+            {activeRooms.map(room => (
+                <ActiveRoomButton 
+                    roomId={room.roomId}
+                    creatorUsername={room.creatorUsername}
+                    amountOfParticipants={room.participants.length}
+                    key={room.roomId}
+                    isUserInRoom={isUserInRoom}
+                />
+            ))}
         </MainContainer>
 
     );
 };
 
-export default SideBar;
+const mapStoreStateToProps = ({ room }) => {
+    return {
+        ...room,
+    };
+};
+
+export default connect(mapStoreStateToProps)(SideBar);
